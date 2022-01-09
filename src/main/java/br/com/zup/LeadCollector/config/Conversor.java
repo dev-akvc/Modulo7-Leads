0b28644008c6539b0ffd043621bad9aa878aae64
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 01:32:49 -0300

    Criar Component para ModelMapper

--- src/main/java/br/com/zup/LeadCollector/config/Conversor.java
+++ src/main/java/br/com/zup/LeadCollector/config/Conversor.java
@@ -1,2 +1,14 @@
-package br.com.zup.LeadCollector.config;public class Conversor {
+package br.com.zup.LeadCollector.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return  new ModelMapper();
+    }
 }
