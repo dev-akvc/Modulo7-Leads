3414f5fb8c185635046c4437901ad559d6dbb032
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 20:42:43 -0300

    Adicionar query

---
+++ src/main/java/br/com/zup/LeadCollector/config/security/UsuarioLoginService.java
@@ -0,0 +1,2 @@
+package br.com.zup.LeadCollector.config.security;public class UsuarioLoginService {
+}
--- src/main/java/br/com/zup/LeadCollector/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioRepository.java
@@ -2,7 +2,10 @@ package br.com.zup.LeadCollector.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends CrudRepository <Usuario, UUID>{
+    Optional<Usuario> findByEmail(String email);
+
 }
