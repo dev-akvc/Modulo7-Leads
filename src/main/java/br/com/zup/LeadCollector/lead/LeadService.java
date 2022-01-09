4585efb90507d8e174843d9b7ed5a44618e7205f
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 02:01:38 -0300

    Criar classe Lead Service com metodo para salvar lead

--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -1,2 +1,16 @@
-package br.com.zup.LeadCollector.lead;public class LeadService {
+package br.com.zup.LeadCollector.lead;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeadService {
+
+    @Autowired
+    private LeadRepository leadRepository;
+
+    public Lead salvarLead(Lead lead){
+        return leadRepository.save(lead);
+    }
+
 }
