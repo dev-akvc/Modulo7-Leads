64b2f82512ebbe4b2b4b1d4499d126ded7c05db5
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 16:49:13 -0300

    Adicionar politica de sessao Stateless

--- src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoDeSeguranca.java
@@ -1,10 +1,15 @@
 package br.com.zup.LeadCollector.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,8 +23,17 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.cors().configurationSource(configurarCors());
 
         http.authorizeRequests().antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO).permitAll().anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource configurarCors(){
+        UrlBasedCorsConfigurationSource cors = new UrlBasedCorsConfigurationSource();
+        cors.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return cors;
     }
 
 }
