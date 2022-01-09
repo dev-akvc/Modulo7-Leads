248cf2925b2357ece76ea54f3acac362aaf42562
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 02:00:36 -0300

    Criar interface Lead Repository

--- src/main/java/br/com/zup/LeadCollector/lead/LeadRepository.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadRepository.java
@@ -1,2 +1,7 @@
-package br.com.zup.LeadCollector.lead;public class LeadRepository {
+package br.com.zup.LeadCollector.lead;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LeadRepository extends CrudRepository <Lead, String>{
+
 }
