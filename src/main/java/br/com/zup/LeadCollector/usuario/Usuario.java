1f9df002c1ddb07be55bb73b6b7fcfb837a054a9
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 01:05:09 -0300

    Criar classe Usuario

--- src/main/java/br/com/zup/LeadCollector/usuario/Usuario.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/Usuario.java
@@ -1,2 +1,19 @@
-package br.com.zup.LeadCollector.usuario;public class Usuario {
+package br.com.zup.LeadCollector.usuario;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
 }
