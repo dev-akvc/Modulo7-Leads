5c8e63336d1f3c12a01249c4f7ef1b4aac366183
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 20:44:48 -0300

    Criar classe service para Login de Usuario

--- src/main/java/br/com/zup/LeadCollector/config/security/UsuarioLoginService.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/UsuarioLoginService.java
@@ -1,2 +1,29 @@
-package br.com.zup.LeadCollector.config.security;public class UsuarioLoginService {
+package br.com.zup.LeadCollector.config.security;
+
+import br.com.zup.LeadCollector.usuario.Usuario;
+import br.com.zup.LeadCollector.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioLoginService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+
+        usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Email ou senha incorreto"));
+        Usuario usuario = usuarioOptional.get();
+
+        return new UsuarioLogado(usuario.getId(), usuario.getEmail(), usuario.getSenha());
+    }
+
 }
