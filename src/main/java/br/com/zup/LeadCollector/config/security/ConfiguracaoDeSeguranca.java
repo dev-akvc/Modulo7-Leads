abd3da151db6a26fdd3702e92c0ccae185cf3b6a
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 16:20:08 -0300

    Criar metodo para configurar permissao de acesso a endpoints

--- src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoDeSeguranca.java
@@ -1,11 +1,25 @@
 package br.com.zup.LeadCollector.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    private static final String[] ENDPOINT_POST_PUBLICO = {
+            "/leads",
+            "/usuario"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests().antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO).permitAll().anyRequest().authenticated();
+    }
 
 }
