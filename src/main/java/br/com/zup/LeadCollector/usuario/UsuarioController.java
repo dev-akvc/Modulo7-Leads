8bde78379f67197b271159af67e957b49e1f3618
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 01:42:56 -0300

    Criar metodo para cadastrar usuario

--- src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
@@ -1,8 +1,11 @@
 package br.com.zup.LeadCollector.usuario;
 
+import br.com.zup.LeadCollector.usuario.dtos.CadastroUsuarioDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -10,5 +13,16 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarUsuario(@RequestBody CadastroUsuarioDTO cadastroUsuarioDTO){
+        Usuario usuario = modelMapper.map(cadastroUsuarioDTO, Usuario.class);
+
+        usuarioService.salvarUsuario(usuario);
+    }
 
 }
