0baef8c181543e5679850324d78f418fa5c7a13f
Anna Karolyne <dev.akvc@gmail.com>
2022-01-09 01:42:13 -0300

    Alterar estrategia de gerar id

--- src/main/java/br/com/zup/LeadCollector/usuario/Usuario.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/Usuario.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     @Column(nullable = false)
     private String email;
