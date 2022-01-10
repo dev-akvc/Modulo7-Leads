a8ebf7d80d118f9283a85a0256a8fe4f4f75f74a
Anna Karolyne <dev.akvc@gmail.com>
2022-01-10 00:43:01 -0300

    Implementar atualizacao de senha para usuario

--- src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioController.java
@@ -1,9 +1,12 @@
 package br.com.zup.LeadCollector.usuario;
 
+import br.com.zup.LeadCollector.config.security.UsuarioLogado;
 import br.com.zup.LeadCollector.usuario.dtos.CadastroUsuarioDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +26,14 @@ public class UsuarioController {
         Usuario usuario = modelMapper.map(cadastroUsuarioDTO, Usuario.class);
 
         usuarioService.salvarUsuario(usuario);
+    }
+
+    @PutMapping
+    public void atualizarUsuario(@RequestBody CadastroUsuarioDTO cadastroUsuarioDTO, Authentication authentication){
+        UsuarioLogado usuarioLogado = (UsuarioLogado) authentication.getPrincipal();
+
+        Usuario usuario = modelMapper.map(cadastroUsuarioDTO, Usuario.class);
+        usuarioService.atualizarUsuario(usuario, usuarioLogado.getId());
     }
 
 }
--- src/main/java/br/com/zup/LeadCollector/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioService.java
@@ -1,8 +1,12 @@
 package br.com.zup.LeadCollector.usuario;
 
+import br.com.zup.LeadCollector.usuario.exceptions.UsuarioNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UsuarioService {
@@ -16,6 +20,21 @@ public class UsuarioService {
     public Usuario salvarUsuario(Usuario usuario){
         usuario.setSenha(encoder.encode(usuario.getSenha()));
         return usuarioRepository.save(usuario);
+    }
+
+    public void atualizarUsuario(Usuario usuario, UUID id){
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+        if(usuarioOptional.isEmpty()){
+            throw new UsuarioNaoEncontradoException();
+        }
+        Usuario usuarioBanco = usuarioOptional.get();
+        if (!usuarioBanco.getEmail().equalsIgnoreCase(usuario.getEmail())){
+            usuarioBanco.setEmail(usuario.getEmail());
+        }
+
+        String senhaEscondida = encoder.encode(usuario.getSenha());
+        usuarioBanco.setSenha(senhaEscondida);
+        usuarioRepository.save(usuarioBanco);
     }
 
 }
